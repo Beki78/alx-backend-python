cf69b8978a0c614d48b847a7677101b760dcf7ec
Bereket Getachew <bakiget78@gmail.com>
2025-01-19 21:53:30 +0300

    first commit

--- Django-Middleware-0x03/0x03-MessagingApp-Django/chats/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsParticipant(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return request.user in obj.participants.all()
-
-class IsSender(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return request.user == obj.sender
