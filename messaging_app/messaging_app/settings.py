b8a4693064d369ca5d8676e54589494d094a5d96
Bereket Getachew <bakiget78@gmail.com>
2025-01-26 15:33:11 +0300

    adding a docker-compose.yaml

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -1,5 +1,5 @@
 
-
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -91,7 +91,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('MYSQL_DB'),
+        'USER': os.getenv('MYSQL_USER'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': os.getenv('MYSQL_HOST', 'db'),  # The name of the service
+        'PORT': '3306',
+    }
+}
 
 DATABASES = {
     'default': {
